d94885b95c8a28d0cb8ac25eb75d87ee65ea229b
seattle2010 <shitn2000@163.com>
2017-06-21 21:28:12 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,21 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("hello"); 
         System.out.println(); 
-        System.out.println("hello"); 
+        System.out.println("hello");
+
+        //while 和 dowhile 的区别
+        boolean boo = false;
+        while(boo){
+
+            System.out.println(boo);//词句不执行
+        }
+
+        do {
+            System.out.println("hello do while");//执行了一次
+
+        } while ( boo);
+
+
+
     }
 }
