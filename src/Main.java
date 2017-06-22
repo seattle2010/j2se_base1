92f071890ecd9f337e9ab7b6ec0c2e86d4431f11
seattle2010 <shitn2000@163.com>
2017-06-22 19:47:33 +0800

    20170622

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,22 @@ public class Main {
     public static void main(String[] args) {
         
         System.out.println("Hello World!");
-        System.out.println("hello"); 
-        System.out.println(); 
-        System.out.println("hello"); 
+        byte b = 10;
+        int i = b;
+        System.out.println(i); 
+        
+        byte b2 = (byte)200;
+        System.out.println(b2);
+
+        int i1 = 5;
+        int j1 = i1++;
+        System.out.println(j1);
+        System.out.println(i1);
+
+       int m = 5;
+        int n = ++m;
+        System.out.println(m);
+        System.out.println(n);
+
     }
 }
